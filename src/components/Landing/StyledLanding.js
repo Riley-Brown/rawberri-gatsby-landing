26de81cebfbacd6301ce7b6af1700e5f4de397fe
Riley Brown <riley@riley.gg>
2019-10-27 17:31:56 -0400

    added styles and animation for landing content

--- src/components/Landing/StyledLanding.js
+++ src/components/Landing/StyledLanding.js
@@ -5,8 +5,54 @@ import { transitionClasses } from "helpers/transitionClasses"
 export const StyledLanding = styled.div`
   ${transitionClasses("fade", 1000)};
   height: calc(100vh - 101px);
-  background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5));
+  background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7));
   position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .landing-content {
+    color: #fff;
+    animation: 1s transformIn ease-out;
+    @keyframes transformIn {
+      from {
+        opacity: 0;
+        transform: translateY(-40px);
+      }
+      to {
+        opacity: 1;
+        transform: translateY(0px);
+      }
+    }
+    h1 {
+      font-size: 5rem;
+      text-transform: uppercase;
+      font-weight: 200;
+      span {
+        font-size: inherit;
+        font-weight: bold;
+      }
+    }
+    h6 {
+      font-size: 3rem;
+      text-align: center;
+      font-weight: bold;
+    }
+    .buttons {
+      text-align: center;
+      margin: 20px 0;
+      button {
+        background: #fff;
+        color: var(--secondary);
+        padding: 10px 25px;
+        font-size: 2.3rem;
+        margin: 0 15px;
+        &:last-of-type {
+          background: var(--secondary);
+          color: #fff;
+        }
+      }
+    }
+  }
   .overlay {
     position: absolute;
     top: 0;
