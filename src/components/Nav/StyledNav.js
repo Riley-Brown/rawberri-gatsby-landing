154c92dad5137b6b812a82e5d131b84024b33ec3
Riley Brown <riley@riley.gg>
2019-10-26 20:45:19 -0400

    added styles for navbar menu items

--- src/components/Nav/StyledNav.js
+++ src/components/Nav/StyledNav.js
@@ -1,19 +1,38 @@
 import styled from "styled-components"
 
 export const StyledNav = styled.nav`
-  margin-bottom: 1.45rem;
   position: sticky;
   top: 0;
-  background: #fff;
   z-index: 1;
   box-shadow: 2px 4px 10px rgba(0, 0, 0, 0.2);
-  text-align: center;
   @media (max-width: 768px) {
     position: static;
   }
   .nav-container {
-    max-width: 1400;
+    max-width: 1400px;
     padding: 1.45rem 1.0875rem;
+    width: 80%;
+    margin: auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    ul {
+      display: flex;
+      list-style: none;
+      align-items: center;
+      * {
+        color: var(--secondary);
+      }
+      li {
+        margin: 0 5px;
+        padding: 10px;
+        cursor: pointer;
+        font-size: 2rem;
+        a {
+          text-decoration: none;
+        }
+      }
+    }
     img {
       height: 70px;
       @media (max-width: 960px) {
