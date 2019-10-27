36eac5e593bf2406ebf171f0d9149f0c24c1c6e0
Riley Brown <riley@riley.gg>
2019-10-26 20:46:33 -0400

    added nav bar menu items

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,7 +1,10 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import logo from "assets/img/rawberri-logo-cropped-bg.png"
+import logo from "assets/img/logo-no-bg.png"
+import twitterSvg from "assets/svg/twitter.svg"
+import instagramSvg from "assets/svg/instagram.svg"
+import grubHubSvg from "assets/svg/grub-hub.svg"
 
 import { StyledNav } from "./StyledNav"
 
@@ -11,6 +14,24 @@ const Nav = ({ siteTitle }) => (
       <Link to="/">
         <img src={logo} />
       </Link>
+      <ul>
+        <li>
+          {/* <img src={twitterSvg} alt="" /> */}
+          Home
+        </li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Menu</li>
+        <li>
+          <a href="https://wizardly-jang-d732c8.netlify.com/">Store</a>
+        </li>
+        <li>
+          <a href="https://loving-kilby-fc3f0a.netlify.com/">Order Online</a>
+        </li>
+
+        {/* <img src={instagramSvg} alt="" /> */}
+        {/* <img src={grubHubSvg} alt="" /> */}
+      </ul>
     </div>
   </StyledNav>
 )
