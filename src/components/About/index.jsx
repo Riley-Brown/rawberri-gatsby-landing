c56c4a5d7499e3b235934fe7b9fc60ba0b87e6e9
Riley Brown <riley@riley.gg>
2019-11-02 22:43:02 -0400

    add isVisible hook to tell when image is invite view

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,8 +1,10 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useLayoutEffect, useRef } from "react"
 import { StyledAbout } from "./StyledAbout"
 
 import rawberriOne from "assets/img/rawberri-inside-1.jpg"
 import rawberriYogurt from "assets/img/rawberri-yogurt.jpg"
+
+import { useIsVisible } from "hooks/useIsVisible"
 
 export default function About() {
   const [loaded, setLoaded] = useState(false)
@@ -10,6 +12,14 @@ export default function About() {
   useEffect(() => {
     setLoaded(true)
   }, [])
+
+  const leftRef = useRef()
+  const rightRef = useRef()
+
+  const leftVisible = useIsVisible({ ref: leftRef, rootMargin: "-200px" })
+  const rightVisible = useIsVisible({ ref: rightRef, rootMargin: "-200px" })
+
+  console.log(leftVisible, rightVisible)
 
   return (
     <StyledAbout loaded={loaded}>
@@ -26,11 +36,11 @@ export default function About() {
         </div>
       </div>
       <div className="image-container">
-        <img data-right src={rawberriOne} alt="" />
+        <img data-right src={rawberriOne} ref={rightRef} alt="" />
         <span data-right className="overlay" />
       </div>
       <div className="image-container">
-        <img data-left src={rawberriYogurt} alt="" />
+        <img data-left src={rawberriYogurt} ref={leftRef} alt="" />
         <span data-left className="overlay" />
       </div>
       <div className="about-content">
